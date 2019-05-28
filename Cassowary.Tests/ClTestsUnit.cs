05d29841ab945196b5aa409df5e5f023f0b136af
Marius Orfgen <marius.orfgen@minitec.de>
2019-05-28 15:09:19 +0200

    Moved to .NET 4.6.1

---
+++ Cassowary.Tests/ClTestsUnit.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace Cassowary.Tests
+{
+    [TestFixture]
+    public class ClTestsUnit
+    {
+        [Test]
+        public void Simple1()
+        {
+            Assert.IsTrue(ClTests.Simple1());
+        }
+
+        [Test]
+        public void JustStay1()
+        {
+            Assert.IsTrue(ClTests.JustStay1());
+        }
+
+        [Test]
+        public void AddDelete1()
+        {
+            Assert.IsTrue(ClTests.AddDelete1());
+        }
+
+        [Test]
+        public void AddDelete2()
+        {
+            Assert.IsTrue(ClTests.AddDelete2());
+        }
+
+        [Test]
+        public void Casso1()
+        {
+            Assert.IsTrue(ClTests.Casso1());
+        }
+
+        [Test]
+        public void Inconsistent1o1()
+        {
+            Assert.IsTrue(ClTests.Inconsistent1());
+        }
+
+        [Test]
+        public void Inconsistent2()
+        {
+            Assert.IsTrue(ClTests.Inconsistent2());
+        }
+
+        [Test]
+        public void Inconsistent3()
+        {
+            Assert.IsTrue(ClTests.Inconsistent3());
+        }
+
+        [Test]
+        public void Multiedit()
+        {
+            Assert.IsTrue(ClTests.Multiedit());
+        }
+    }
+}
---
+++ Cassowary.Tests/LayoutTestUnit.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Cassowary.Tests
+{
+    [TestFixture]
+    public class LayoutTestUnit
+    {
+        [Test]
+        public void TestMethod()
+        {
+            LayoutTest._solver = new ClSimplexSolver();
+
+            // initialize the needed variables
+            LayoutTest.BuildVariables();
+
+            // create the constraints
+            try
+            {
+                LayoutTest.BuildConstraints();
+            }
+            catch (ExClRequiredFailure rf)
+            {
+                Console.Error.WriteLine(rf.StackTrace);
+            }
+            catch (ExClInternalError ie)
+            {
+                Console.Error.WriteLine(ie.StackTrace);
+            }
+
+            // solve it
+            try
+            {
+                LayoutTest._solver.Solve();
+            }
+            catch (ExClInternalError ie)
+            {
+                Console.Error.WriteLine(ie.StackTrace);
+            }
+
+            // print out the values
+            Console.WriteLine("Variables: ");
+            LayoutTest.PrintVariables();
+
+        }
+    }
+}
